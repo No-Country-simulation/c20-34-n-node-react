b0dff96b31b4a6713fb4749027c0b7425985e871
Luciano Pardo <pardobio@gmail.com>
2024-09-16 23:12:14 -0300

    three js instalation update

--- Front/src/components/Register.jsx
+++ Front/src/components/Register.jsx
@@ -47,12 +47,15 @@ const Register = () => {
 
     return (
         <div className="flex items-center justify-center h-screen w-screen bg-gray-200">
+
             <div className="flex w-3/4 bg-white shadow-md rounded-lg overflow-hidden">
+
                 <div className="w-1/2 bg-gray-300 flex items-center justify-center">
                     <div className="w-64 h-64 bg-gray-400 rounded-lg flex items-center justify-center">
                         <img src="your-image-url.png" alt="Imagen de demostración" />
                     </div>
                 </div>
+
                 <div className="w-1/2 p-10 flex flex-col items-center">
                     <form className="w-full" onSubmit={handleSubmit}>
                         <div className="mb-4">
@@ -144,59 +147,8 @@ const Register = () => {
                     </a>
                 </div>
             </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-gray-700">
-                Contraseña
-              </label>
-              <input
-                id="password"
-                name="password"
-                className="w-full p-3 rounded-lg border border-gray-300"
-                type="password"
-                placeholder="Ingrese su contraseña"
-                aria-label="Contraseña"
-                title="Contraseña"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="role" className="block text-gray-700">
-                Rol
-              </label>
-              <select
-                id="role"
-                name="role"
-                className="w-full p-3 rounded-lg border border-gray-300"
-                aria-label="Rol"
-                title="Rol"
-                value={formData.role}
-                onChange={handleChange}
-                required
-              >
-                <option value="" disabled>
-                  Seleccione su rol
-                </option>
-                <option value="user">Usuario</option>
-                <option value="admin">Administrador</option>
-              </select>
-            </div>
-            <button
-              type="submit"
-              className="w-full p-3 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
-            >
-              REGISTRARSE
-            </button>
-          </form>
-
-          <a href="/login" className="text-gray-500 hover:text-gray-700">
-            Go to login
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+        </div >
+    );
 };
 
 export default Register;
